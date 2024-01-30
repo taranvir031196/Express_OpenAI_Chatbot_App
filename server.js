bad59f5faaf78a51e3c81927ae51a4d025867ca0
Taranvir Singh Saini <taranvir.554@gmail.com>
2024-01-30 19:44:42 +0530

    Added files

---
+++ server.js
@@ -0,0 +1,35 @@
+const { Configuration, OpenAIApi } = require("openai")
+const express = require('express')
+var bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+const app = express()
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+const openai = new OpenAIApi(new Configuration({
+    //replace your-api key with your API key from ChatGPT
+    apiKey: process.env.API_KEY
+}));
+
+app.post("/chat", async (req, res) => {
+    try {
+        const resp = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "user", content: req.body.question }
+            ]
+        })
+
+        res.status(200).json({ message: resp.data.choices[0].message.content })
+    } catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+})
+app.listen(process.env.PORT, () => {
+    console.log(`OpenAI Chatbot is active on ${process.env.PORT}`)
+})
